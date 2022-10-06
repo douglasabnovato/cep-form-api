078147c6e1281a4ff4daaef8e3d3e328e6b986d7
Douglas A B Novato <douglasabnovato@gmail.com>
2022-10-06 16:50:45 -0300

    revisando o código

--- validation.js
+++ validation.js
@@ -1,53 +1,76 @@
 const form = document.getElementById("form");
 const username = document.getElementById("txtName");
-console.log("form",form)
-console.log("username",username)
-
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const passwordtwo = document.getElementById("password-two");
+const cep = document.getElementById("txtCep");
+const street = document.getElementById("txtStreet");
+const numberStr = document.getElementById("txtNumber");
+const complement = document.getElementById("txtComplement");
+const neighborhood = document.getElementById("txtNeighborhood");
+const city = document.getElementById("txtCity");
+const state = document.getElementById("txtState");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("username addEventListener",username)
   checkInputs();
 });
 
 function checkInputs() {
   const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const passwordtwoValue = passwordtwo.value.trim();
+  const cepValue = cep.value.trim();
+  const streetValue = street.value.trim();
+  const numberStrValue = numberStr.value.trim();
+  const complementValue = complement.value.trim();
+  const neighborhoodValue = neighborhood.value.trim();
+  const cityValue = city.value.trim();
+  const stateValue = state.value.trim();
 
   if (usernameValue === "") {
-    setErrorFor(username, "Esse campo é obrigatório.");
+    setErrorFor(username, "Esse campo é obrigatório!");
   } else {
     setSuccessFor(username);
   }
 
-  if (emailValue === "") {
-    setErrorFor(email, "Preencha esse campo");
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, "Email inválido");
+  if (cepValue === "") {
+    setErrorFor(cep, "Esse campo é obrigatório!");
   } else {
-    setSuccessFor(email);
+    setSuccessFor(cep);
   }
 
-  if (passwordValue === "") {
-    setErrorFor(password, "Preencha esse campo");
-  } else if (passwordValue.length < 8) {
-    setErrorFor(password, "Senha deve ter mais que 8 caracteres");
+  if (streetValue === "") {
+    setErrorFor(street, "Esse campo é obrigatório!");
   } else {
-    setSuccessFor(password);
+    setSuccessFor(street);
   }
 
-  if (passwordtwoValue === "") {
-    setErrorFor(passwordtwo, "Preencha esse campo");
-  } else if (passwordValue !== passwordtwoValue) {
-    setErrorFor(passwordtwo, "Senhas não tão iguais");
+  if (numberStrValue === "") {
+    setErrorFor(numberStr, "Esse campo é obrigatório!");
   } else {
-    setSuccessFor(passwordtwo);
+    setSuccessFor(numberStr);
   }
+
+  if (complementValue === "") {
+    setErrorFor(complement, "Esse campo é obrigatório!");
+  } else {
+    setSuccessFor(complement);
+  }
+
+  if (neighborhoodValue === "") {
+    setErrorFor(neighborhood, "Esse campo é obrigatório!");
+  } else {
+    setSuccessFor(neighborhood);
+  }
+
+  if (cityValue === "") {
+    setErrorFor(city, "Esse campo é obrigatório!");
+  } else {
+    setSuccessFor(city);
+  }
+
+  if (stateValue === "") {
+    setErrorFor(state, "Esse campo é obrigatório!");
+  } else {
+    setSuccessFor(state);
+  }
+
 }
 
 function setErrorFor(input, message) {
@@ -62,8 +85,3 @@ function setSuccessFor(input) {
   formControl.className = "form-control success";
 }
 
-function isEmail(email) {
-  return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
-    email
-  );
-}
